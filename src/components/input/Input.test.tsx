ba8cb892963f1bbc407b9ec7bb6d3387066c2fe2
Gonzalo Jusid <gonzalojusid@gmail.com>
2023-04-06 19:33:27 -0300

    Added test config

---
+++ src/components/input/Input.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import Input from "./Input";
+
+describe("Input", () => {
+    it("should work as expected", () => {
+        // render(<App />);
+        expect(1 + 1).toBe(2);
+    });
+});
